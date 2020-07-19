99a3106981ad9822ed6bac7d71c80ae0049c7dff
Jonathan Gil <jhongilp@gmail.com>
2020-07-19 17:05:59 -0500

    implement next_head method

--- src/snake.rs
+++ src/snake.rs
@@ -6,6 +6,7 @@ use crate::draw::draw_block;
 
 const SNAKE_COLOR: Color = [0.00, 0.00, 0.00, 1.0];
 
+#[derive(Copy, Clone, PartialEq)]
 pub enum Direction {
   Up,
   Down,
@@ -24,6 +25,7 @@ impl Direction {
   }
 }
 
+#[derive(Debug, Clone)]
 struct Block {
   x: i32,
   y: i32,
@@ -94,5 +96,25 @@ impl Snake {
     self.tail = Some(removed_block);
   }
 
+  pub fn head_direction(&self) -> Direction {
+    self.direction
+  }
+
+  pub fn next_head(&self, dir: Option<Direction>) -> (i32, i32) {
+    let (head_x, head_y): (i32, i32) = self.head_position();
+
+    let mut moving_dir = self.direction;
+    match dir {
+      Some(d) => moving_dir = d,
+      None => {}
+    }
+
+    match moving_dir {
+      Direction::Up => (head_x, head_y - 1),
+      Direction::Down => (head_x, head_y + 1),
+      Direction::Left => (head_x - 1, head_y),
+      Direction::Right => (head_x + 1, head_y),
+    }
+  }
 
 }
